35713dd06fe74f3d50ce6108335d7fc12dfc9065
Virpinchk <91627169+Virpinchk@users.noreply.github.com>
2022-03-01 17:09:15 +0500

    Add files via upload

--- scripts/LivingEntity.gd
+++ scripts/LivingEntity.gd
@@ -1,16 +1,36 @@
 extends KinematicBody2D
+
 class_name LivingEntity
+
 export var HP: int = 10 setget entity_HP_change
+
 export var max_HP: int = 10
 
 func entity_HP_change(new_value):
 	HP = clamp(new_value,0,max_HP)
+	
+export var speed:float = 200
+export var acceleratioon:float = 2000
+var velocity:Vector2
 
-# Called when the node enters the scene tree for the first time.
+func entity_calculate_target_velocity() ->Vector2:
+	return Vector2()
+	
+func entity_move(target_vet:Vector2, delta:float):
+	velocity = velocity.move_toward(target_vet,acceleratioon*delta)
+	velocity = move_and_slide(velocity)
+
 func _ready():
-	pass # Replace with function body.
+	pass 
 
+enum EntityState{
+		DEAD = 0,
+		INACTIVE = 1,
+		ALIVE = 2,
+		PATROL = 3,
+		CHASE = 4
+}
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+export(EntityState) var state = EntityState.INACTIVE setget new_state 
+func new_state(new_value):
+	state = new_value
